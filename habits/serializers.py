7599676186ce25032a117221d79e16ce190b5003
AlexandrBxbrxv <g1026@bk.ru>
2024-11-09 11:21:39 +0500

    + отображение is_nice_habit только для чтения

--- habits/serializers.py
+++ habits/serializers.py
@@ -5,13 +5,25 @@ from habits.models import Habit
 
 class AssociatedHabitSerializer(serializers.ModelSerializer):
     """Сериализатор связанной привычки для модели Habit."""
+    is_nice_habit = serializers.SerializerMethodField(read_only=True)
+
     class Meta:
         model = Habit
-        exclude = ('reward',)
+        exclude = ('associated_habit', 'reward',)
+
+    @staticmethod
+    def get_is_nice_habit(habit):
+        return habit.is_nice_habit
 
 
 class HabitSerializer(serializers.ModelSerializer):
     """Сериализатор для модели Habit."""
+    is_nice_habit = serializers.SerializerMethodField(read_only=True)
+
     class Meta:
         model = Habit
-        exclude = ('is_nice_habit',)
+        fields = '__all__'
+
+    @staticmethod
+    def get_is_nice_habit(habit):
+        return habit.is_nice_habit

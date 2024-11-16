ffea3ce1023ce7c632a151cf70e5d9868c06fb9e
AlexandrBxbrxv <g1026@bk.ru>
2024-11-16 15:12:38 +0500

    + поле user теперь только для чтения

--- habits/serializers.py
+++ habits/serializers.py
@@ -10,6 +10,7 @@ class HabitSerializer(serializers.ModelSerializer):
     class Meta:
         model = Habit
         fields = '__all__'
+        read_only_fields = ('user',)
         validators = [
             HabitValidator(is_nice_habit='is_nice_habit', associated_habit='associated_habit', reward='reward')
         ]

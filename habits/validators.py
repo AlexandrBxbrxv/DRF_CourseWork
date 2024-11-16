2e050ea7c731edc7e403a7fe582ca329ecef8b1d
AlexandrBxbrxv <g1026@bk.ru>
2024-11-16 21:41:15 +0500

    + проверка associated_habit не ссылается на полезную привычку

--- habits/validators.py
+++ habits/validators.py
@@ -1,8 +1,11 @@
 from rest_framework import serializers
 
+from habits.models import Habit
+
 
 class HabitValidator:
-    """Проверяет что поля associated_habit и reward не заполнены одновременно."""
+    """Проверяет что поля associated_habit и reward не заполнены одновременно,
+     максимальное время на выполнение не больше 120, associated_habit не ссылается на полезную привычку."""
 
     def __call__(self, value):
         self.is_nice_habit = dict(value).get("is_nice_habit")
@@ -26,3 +29,9 @@ class HabitValidator:
         if self.time_for_execution > 120:
             message = 'Время на выполнение не должно превышать 120 секунд.'
             raise serializers.ValidationError(message)
+
+        if self.associated_habit is not None:
+            self.is_associated_habit_is_nice_habit = Habit(self.associated_habit).is_nice_habit
+            if not self.is_associated_habit_is_nice_habit:
+                message = 'Связанная привычка должна ссылаться на приятную привычку.'
+                raise serializers.ValidationError(message)

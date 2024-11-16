1d6263918b0ddeaaed7168578919c849cb0d30b1
AlexandrBxbrxv <g1026@bk.ru>
2024-11-16 21:57:12 +0500

    fix permissions

--- users/urls.py
+++ users/urls.py
@@ -1,3 +1,5 @@
+from rest_framework.permissions import AllowAny
+
 from users.apps import UsersConfig
 from django.urls import path
 
@@ -11,7 +13,7 @@ from users.views import UserCreateAPIView
 app_name = UsersConfig.name
 
 urlpatterns = [
-    path('login/', TokenObtainPairView.as_view(), name='login'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('login/', TokenObtainPairView.as_view(permission_classes=(AllowAny,)), name='login'),
+    path('token/refresh/', TokenRefreshView.as_view(permission_classes=(AllowAny,)), name='token_refresh'),
     path('register/', UserCreateAPIView.as_view(), name='register'),
 ]

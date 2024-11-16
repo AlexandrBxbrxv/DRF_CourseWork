5538c33237747f9c23d74201a792c46fab6bcd89
AlexandrBxbrxv <g1026@bk.ru>
2024-11-16 14:54:42 +0500

    fix

--- habits/validators.py
+++ habits/validators.py
@@ -8,14 +8,18 @@ class HabitValidator:
         self.associated_habit = associated_habit
         self.reward = reward
 
-    def __call__(self, is_nice_habit, associated_habit, reward):
-        if is_nice_habit:
-            if associated_habit or reward is not None:
+    def __call__(self, value):
+        self.is_nice_habit = value.get(self.is_nice_habit)
+        self.associated_habit = value.get(self.associated_habit)
+        self.reward = value.get(self.reward)
+
+        if self.is_nice_habit:
+            if self.associated_habit or self.reward is not None:
                 message = 'Приятная привычка не может содержать связанной привычки или вознаграждения.'
                 raise serializers.ValidationError(message)
 
-        if not associated_habit:
-            if associated_habit and reward is not None:
+        if not self.is_nice_habit:
+            if self.associated_habit and self.reward is not None:
                 message = 'Привычка не может содержать и связанную привычку и вознаграждение одновременно.'
                 raise serializers.ValidationError(message)
 

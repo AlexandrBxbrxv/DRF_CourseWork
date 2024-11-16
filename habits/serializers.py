7ee790dda09a7369959da0086870ef8f4c0eb4b6
AlexandrBxbrxv <g1026@bk.ru>
2024-11-16 14:06:28 +0500

    + сериализатор для Habit

--- habits/serializers.py
+++ habits/serializers.py
@@ -1,29 +1,15 @@
 from rest_framework import serializers
 
 from habits.models import Habit
-
-
-class AssociatedHabitSerializer(serializers.ModelSerializer):
-    """Сериализатор связанной привычки для модели Habit."""
-    is_nice_habit = serializers.SerializerMethodField(read_only=True)
-
-    class Meta:
-        model = Habit
-        exclude = ('associated_habit', 'reward',)
-
-    @staticmethod
-    def get_is_nice_habit(habit):
-        return habit.is_nice_habit
+from habits.validators import HabitValidator
 
 
 class HabitSerializer(serializers.ModelSerializer):
     """Сериализатор для модели Habit."""
-    is_nice_habit = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = Habit
         fields = '__all__'
-
-    @staticmethod
-    def get_is_nice_habit(habit):
-        return habit.is_nice_habit
+        validators = [
+            HabitValidator(is_nice_habit='is_nice_habit', associated_habit='associated_habit', reward='reward')
+        ]

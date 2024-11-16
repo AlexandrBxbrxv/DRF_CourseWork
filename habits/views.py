eafd1e2712041f68d1522324c87ca24685bb1f27
AlexandrBxbrxv <g1026@bk.ru>
2024-11-16 15:11:59 +0500

    + присвоение user к Habit

--- habits/views.py
+++ habits/views.py
@@ -8,3 +8,9 @@ class HabitCreateAPIView(generics.CreateAPIView):
     """Контроллер для создания привычки."""
     serializer_class = HabitSerializer
     queryset = Habit.objects.all()
+
+    def perform_create(self, serializer):
+        user = self.request.user
+        habit = serializer.save()
+        habit.user = user
+        habit.save()

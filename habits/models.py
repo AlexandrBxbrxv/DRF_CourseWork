970cd065c62f56f5307acc6d19eb2e0929863074
AlexandrBxbrxv <g1026@bk.ru>
2024-11-20 16:25:31 +0500

    + поле next_reminder

--- habits/models.py
+++ habits/models.py
@@ -6,13 +6,13 @@ from users.models import User
 class Habit(models.Model):
 
     PERIODICITY_CHOICES = [
-        ('1_day', 'Через 1 день'),
-        ('2_day', 'Через 2 дня'),
-        ('3_day', 'Через 3 дня'),
-        ('4_day', 'Через 4 дня'),
-        ('5_day', 'Через 5 дней'),
-        ('6_day', 'Через 6 дней'),
-        ('7_day', 'Через 7 дней'),
+        ('1', 'Через 1 день'),
+        ('2', 'Через 2 дня'),
+        ('3', 'Через 3 дня'),
+        ('4', 'Через 4 дня'),
+        ('5', 'Через 5 дней'),
+        ('6', 'Через 6 дней'),
+        ('7', 'Через 7 дней'),
     ]
 
     user = models.ForeignKey(User, on_delete=models.CASCADE, **NULLABLE, related_name='habits_user',
@@ -24,11 +24,13 @@ class Habit(models.Model):
     is_nice_habit = models.BooleanField(default=False, verbose_name='признак приятной привычки')
     associated_habit = models.ForeignKey("Habit", on_delete=models.SET_NULL, **NULLABLE, related_name='nice_habit',
                                          verbose_name='связанная привычка')
-    periodicity = models.CharField(max_length=5, choices=PERIODICITY_CHOICES, verbose_name='периодичность')
+    periodicity = models.CharField(max_length=1, default='1', choices=PERIODICITY_CHOICES, verbose_name='периодичность')
     reward = models.TextField(**NULLABLE, verbose_name='вознаграждение')
     time_for_execution = models.PositiveSmallIntegerField(default=120, help_text='время на выполнение в секундах',
                                                           verbose_name='время на выполнение')
     is_public = models.BooleanField(default=False, verbose_name='признак публичности')
+
+    next_reminder = models.DateTimeField(**NULLABLE, auto_now=True, verbose_name='дата следующего напоминания')
 
     def __str__(self):
         return f'Привычка {self.user}'
